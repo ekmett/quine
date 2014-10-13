e646420fae811bf80943745433b1d3d0a397b6e1
Edward Kmett <ekmett@gmail.com>
2014-10-13 03:51:24 -0400

    strict static/canvas

--- static/canvas.js
+++ static/canvas.js
@@ -1,7 +1,6 @@
 define(["domReady!"], function define_canvas(dr) {
+  'use strict';
   var canvas = document.getElementById("background");
-  if (!canvas) {
-    throw "missing canvas";
-  }
+  if (!canvas) throw "missing canvas";
   return canvas;
 });
