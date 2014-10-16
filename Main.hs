7b72e42f33b6119e05c290bb50f8a032561daa69
Edward Kmett <ekmett@gmail.com>
2014-10-16 01:16:19 -0400

    event code cleanup

--- Main.hs
+++ Main.hs
@@ -16,7 +16,6 @@ import Graphics.UI.SDL.Event as SDL
 import Graphics.UI.SDL.Types as SDL
 import Graphics.UI.SDL.Video as SDL
 
-
 screenWidth, screenHeight :: CInt
 screenWidth  = 1024
 screenHeight = 768
@@ -24,58 +23,52 @@ screenHeight = 768
 infixl 0 <?>
 (<?>) :: MonadIO m => m a -> String -> m a
 m <?> xs = do
-  liftIO $ putStrLn xs
   a <- m -- todo wrap this with a try
   es <- liftIO (get errors)
   unless (null es) $ do
-    -- liftIO $ putStrLn xs
+    liftIO $ putStrLn xs
     forM_ es $ liftIO . print
   return a
 
 main :: IO ()
-main =
-  withCString "engine" $ \windowName -> do
-    putStr "SDL2 Revision: " >> getRevision >>= (peekCString >=> putStrLn)
-    _ <- SDL.init initFlagEverything
-    _ <- glSetAttribute glAttrContextMajorVersion 4
-    _ <- glSetAttribute glAttrContextMinorVersion 1
-    _ <- glSetAttribute glAttrContextProfileMask glProfileCore
-    window <- createWindow windowName windowPosUndefined windowPosUndefined 1024 768 (windowFlagOpenGL .|. windowFlagShown .|. windowFlagResizable .|. windowFlagAllowHighDPI)
-    _ <- glCreateContext window
-    clearColor $= Color4 0 0 0 1
-    glEnable gl_FRAMEBUFFER_SRGB
-    forever $ do
-      pollEvents handleEvent
-      render
+main = withCString "engine" $ \windowName -> do
+  putStr "SDL2 Revision: " >> getRevision >>= (peekCString >=> putStrLn)
+  _ <- SDL.init initFlagVideo -- Everything
+  -- request some minimums
+  _ <- glSetAttribute glAttrContextMajorVersion 4
+  _ <- glSetAttribute glAttrContextMinorVersion 1
+  _ <- glSetAttribute glAttrRedSize 5
+  _ <- glSetAttribute glAttrGreenSize 5
+  _ <- glSetAttribute glAttrBlueSize 5
+  _ <- glSetAttribute glAttrDepthSize 16
+  _ <- glSetAttribute glAttrDoubleBuffer 1
+
+  _ <- glSetAttribute glAttrContextProfileMask glProfileCore
+  window <- createWindow windowName windowPosUndefined windowPosUndefined 1024 768 (windowFlagOpenGL .|. windowFlagShown .|. windowFlagResizable .|. windowFlagAllowHighDPI)
+  _ <- glCreateContext window
+  alloca $ \iptr -> do
+    red   <- glGetAttribute glAttrRedSize   iptr >> peek iptr
+    green <- glGetAttribute glAttrGreenSize iptr >> peek iptr
+    blue  <- glGetAttribute glAttrBlueSize  iptr  >> peek iptr
+    print ("red", red, "green", green, "blue", blue)
+
+  glEnable gl_FRAMEBUFFER_SRGB
+  forever $ poll >> render
 
 render :: IO ()
 render = do
   clearColor $= Color4 0 0 0 1
 
+shutdown :: IO ()
+shutdown = quit >> exitSuccess
+
+poll :: IO ()
+poll = alloca go where
+  go ep = pollEvent ep >>= \ r -> when (r /= 0) $ do
+    peek ep >>= handleEvent
+    go ep
+
 handleEvent :: SDL.Event -> IO ()
 handleEvent QuitEvent{}                                                    = shutdown
 handleEvent KeyboardEvent{keyboardEventKeysym=Keysym{keysymKeycode = 27 }} = shutdown
 handleEvent e = print e
-
-shutdown :: IO ()
-shutdown = quit >> exitSuccess
-
-pollEvents :: (SDL.Event -> IO ()) -> IO ()
-pollEvents k = alloca go where
-  go ep = pollEvent ep >>= \ r -> when (r /= 0) $ do
-    peek ep >>= k
-    go ep
-
-{-
-loop =
-  do quit <- whileEvents
-     unless quit loop
-
-whileEvents =
-  do event <- pollEvent
-     case event of
-       Quit -> return True
-       NoEvent -> return False
-       _ -> whileEvents
--}
-
