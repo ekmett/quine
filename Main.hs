908d86e9863af186f08cef82270400f1f13f38da
Edward Kmett <ekmett@gmail.com>
2014-10-16 00:55:17 -0400

    set up a basic interaction loop

--- Main.hs
+++ Main.hs
@@ -1,24 +1,25 @@
 module Main where
 
-import Control.Monad (unless, (>=>))
+import Control.Monad hiding (forM_)
 import Control.Monad.IO.Class
 --import Control.Monad.Trans.Class
 import Data.Foldable
 --import Data.Traversable
 import Foreign
 import Foreign.C
+import System.Exit
 import Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL.Raw as GL
 import Graphics.UI.SDL.Basic as SDL
 import Graphics.UI.SDL.Enum  as SDL
---import Graphics.UI.SDL.Event as SDL
---import Graphics.UI.SDL.Types as SDL
+import Graphics.UI.SDL.Event as SDL
+import Graphics.UI.SDL.Types as SDL
 import Graphics.UI.SDL.Video as SDL
 
 
 screenWidth, screenHeight :: CInt
-screenWidth  = 1400
-screenHeight = 1050
+screenWidth  = 1024
+screenHeight = 768
 
 infixl 0 <?>
 (<?>) :: MonadIO m => m a -> String -> m a
@@ -33,16 +34,48 @@ m <?> xs = do
 
 main :: IO ()
 main =
-  alloca $ \windowPointer -> alloca $ \rendererPointer -> do
+  withCString "engine" $ \windowName -> do
     putStr "SDL2 Revision: " >> getRevision >>= (peekCString >=> putStrLn)
     _ <- SDL.init initFlagEverything
     _ <- glSetAttribute glAttrContextMajorVersion 4
     _ <- glSetAttribute glAttrContextMinorVersion 1
     _ <- glSetAttribute glAttrContextProfileMask glProfileCore
-    _ <- createWindowAndRenderer screenWidth screenHeight 0 windowPointer rendererPointer
-    window <- peek windowPointer
-    withCString "engine" $ SDL.setWindowTitle window
- 
-    clearColor $= Color4 0 0 0 1 <?> "set clearColor"
-    glEnable gl_FRAMEBUFFER_SRGB <?> "enable sRGB framebuffer"
-    quit <?> "quit"
+    window <- createWindow windowName windowPosUndefined windowPosUndefined 1024 768 (windowFlagOpenGL .|. windowFlagShown .|. windowFlagResizable .|. windowFlagAllowHighDPI)
+    _ <- glCreateContext window
+    clearColor $= Color4 0 0 0 1
+    glEnable gl_FRAMEBUFFER_SRGB
+    forever $ do
+      pollEvents handleEvent
+      render
+
+render :: IO ()
+render = do
+  clearColor $= Color4 0 0 0 1
+
+handleEvent :: SDL.Event -> IO ()
+handleEvent QuitEvent{}                                                    = shutdown
+handleEvent KeyboardEvent{keyboardEventKeysym=Keysym{keysymKeycode = 27 }} = shutdown
+handleEvent e = print e
+
+shutdown :: IO ()
+shutdown = quit >> exitSuccess
+
+pollEvents :: (SDL.Event -> IO ()) -> IO ()
+pollEvents k = alloca go where
+  go ep = pollEvent ep >>= \ r -> when (r /= 0) $ do
+    peek ep >>= k
+    go ep
+
+{-
+loop =
+  do quit <- whileEvents
+     unless quit loop
+
+whileEvents =
+  do event <- pollEvent
+     case event of
+       Quit -> return True
+       NoEvent -> return False
+       _ -> whileEvents
+-}
+
