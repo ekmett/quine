7c6fb8718d8a5b1ca992f9fe9b7102d3143dbc2e
Edward Kmett <ekmett@gmail.com>
2014-10-26 21:04:02 -0400

    streamline more stuff

--- static/canvas.js
+++
@@ -1,6 +0,0 @@
-define(["die","domReady!"], function canvas(die,dr) {
-  'use strict';
-  var canvas = document.getElementById("background");
-  if (!canvas) die("missing canvas");
-  return canvas;
-});
--- static/rainbow.js
+++
@@ -1,7 +0,0 @@
-define(["program","staged","shader!rainbow.vs","shader!rainbow.fs"],function(program,staged,vs,fs) {
-  return staged(function(gl) {
-    this.pos = 0;
-    this.id = program.link(vs,fs,{"pos":0}).stage().id;
-    this.offset = gl.getUniformLocation(this.id,"offset");
-  });
-});
