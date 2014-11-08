8a0ca6e07b7a57979cdfcc9903337cf365e6dd2f
Edward Kmett <ekmett@gmail.com>
2014-11-08 03:46:57 -0500

    when we hit escape and lose relativeMouseMode stop updating the camera mouselook

--- src/Quine/Camera.hs
+++ src/Quine/Camera.hs
@@ -14,12 +14,16 @@ module Quine.Camera
   , updateCamera
   ) where
 
-import Control.Monad.State.Class
+import Control.Monad (when)
+import Control.Monad.IO.Class
+import Control.Monad.State.Class hiding (get)
 import Control.Lens
 import Data.Default
 import Linear
 import Quine.GL.Types
 import Quine.Input
+import Quine.SDL
+import Quine.StateVar
 
 -- * Camera
 
@@ -34,13 +38,15 @@ makeClassy ''Camera
 instance Default Camera where
   def = Camera (pi/2) 0 0 0 0
 
-updateCamera :: (MonadState s m, HasCamera s, HasInput s) => m ()
+updateCamera :: (MonadState s m, HasCamera s, HasInput s, MonadIO m) => m ()
 updateCamera = do
-  let ysensitivity = pi/180 -- negate for inverted mouse, TODO: take from options
-      xsensitivity = pi/180
   V2 dx dy <- mouseRel <<.= 0
-  pitch %= \x -> max (-pi/2) $ min (pi/2) (x + fromIntegral dy * ysensitivity) -- [-pi/2..pi/2]
-  yaw   %= \y -> fmod (y + fromIntegral dx * xsensitivity) (2*pi)              -- [0..2*pi)
+  active <- get relativeMouseMode
+  when active $ do
+    let ysensitivity = pi/180 -- negate for inverted mouse, TODO: take from options
+        xsensitivity = pi/180
+    pitch %= \x -> max (-pi/2) $ min (pi/2) (x + fromIntegral dy * ysensitivity) -- [-pi/2..pi/2]
+    yaw   %= \y -> fmod (y + fromIntegral dx * xsensitivity) (2*pi)              -- [0..2*pi)
 
 fmod :: Float -> Float -> Float
 fmod a b = b * snd (properFraction $ a / b :: (Int, Float))
