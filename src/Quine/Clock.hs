5b3fcbe2b2d8f4c8eac64ba420a804d866186770
Edward Kmett <ekmett@gmail.com>
2014-11-09 20:30:16 -0500

    type synonyms for Time

--- src/Quine/Clock.hs
+++ src/Quine/Clock.hs
@@ -1,13 +1,18 @@
 module Quine.Clock
   ( now
+  , Time
+  , DeltaTime
   ) where
 
 import Control.Monad.IO.Class
 import Data.Int
 import System.Clock
 
+type Time      = Int64
+type DeltaTime = Int64
+
 -- | A simple monotonic system clock counting in nanoseconds with an undefined origin
-now :: MonadIO m => m Int64
+now :: MonadIO m => m Time
 now = liftIO $ do
   TimeSpec s ns <- getTime Monotonic
   return $ fromIntegral s * 1000000000 + fromIntegral ns
