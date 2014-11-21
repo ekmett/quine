21f62e743a98352bf126c1a4da0025becd83e832
Edward Kmett <ekmett@gmail.com>
2014-11-21 04:54:58 -0500

    started integrating a Frustum type

--- src/Quine/Frustum.hs
+++ src/Quine/Frustum.hs
@@ -1,14 +1,28 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Quine.Frustum
   ( Frustum(..)
+  , OverlapsFrustum(..)
+ -- , buildFrustum
   ) where
 
-data Frustum = Frustum 
-  !Plane !Plane !Plane !Plane !Plane !Plane
-  !Vec3 !Vec3 !Vec3 !Vec3 !Vec3 !Vec3 !Vec3 !Vec3 
+import Control.Lens
+import Data.Data
+import Data.Vector -- .Storable
+import GHC.Generics
+import Linear
+import Prelude hiding (any, all)
+import Quine.Bounding.Box
+import Quine.GL.Types
+import Quine.Plane
 
+data Frustum = Frustum { planes :: Vector Plane, points :: Vector Vec3 }
+  deriving (Show,Eq,Ord,Generic,Typeable,Data)
+
+{-
 -- | @buildFrustum origin direction nearZ farZ fovy aspectRatio@
 buildFrustum :: Vec3 -> Vec3 -> Vec3 -> Float -> Float -> Float -> Float -> Frustum
-buildFrustum origin dir up near far fovy aspect = undefined
+buildFrustum origin dir up near far fovy aspect = undefined -- TODO
   where
     t = tan (fovy*0.5)
     nc = origin + near*^dir
@@ -17,24 +31,20 @@ buildFrustum origin dir up near far fovy aspect = undefined
     fh = t * far
     nw = nh * aspect
     fw = fh * aspect
-
-planes :: Frustum -> [Plane]
-planes (Frustum a b c d e f _ _ _ _ _ _ _ _) = [a,b,c,d,e,f]
-
-points :: Frustum -> [Vec3]
-points (Frustum _ _ _ _ _ _ a b c d e f g h) = [a,b,c,d,e,f,g,h]
-
-mix :: V3 a -> V3 a -> V3 a
-mix (V3 a b c) (V3 d e f) = V3 <$> [a,d] <*> [b,e] <*> [c,f] 
+-}
 
 instance OverlapsBox Frustum where
-  overlapsBox fm (Box l h) = not $
-      any (\p -> all (\q -> signedDistance p q < 0) (mix l h)) ps
-   || all (\p -> p^._x > h^._x) qs
-   || all (\p -> p^._y > h^._y) qs
-   || all (\p -> p^._z > l^._z) qs
-   || all (\p -> p^._x < l^._x) qs
-   || all (\p -> p^._y < l^._y) qs
-   || all (\p -> p^._z < l^._z) qs
-   where ps = planes fm
-         qs = points fm
+  overlapsBox (Frustum ps qs) b@(Box l h)
+    = all (\p -> signedDistance p (pVertex p b) >= 0) ps
+   && any (\q -> q^._x <= h^._x) qs
+   && any (\q -> q^._y <= h^._y) qs
+   && any (\q -> q^._z <= h^._z) qs
+   && any (\q -> q^._x >= l^._x) qs
+   && any (\q -> q^._y >= l^._y) qs
+   && any (\q -> q^._z >= l^._z) qs
+
+class OverlapsFrustum a where
+  overlapsFrustum :: a -> Frustum -> Bool
+
+instance OverlapsFrustum Box where
+  overlapsFrustum = flip overlapsBox
