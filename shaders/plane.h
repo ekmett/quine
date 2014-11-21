df66bd1b3c78d9856d8e5f002fbd9c1030af9644
Edward Kmett <ekmett@gmail.com>
2014-11-20 21:53:52 -0500

    fast distance to aabb test, added distance to sphere test

--- shaders/plane.h
+++ shaders/plane.h
@@ -1,6 +1,9 @@
 #ifndef INCLUDED_PLANE_H
 #define INCLUDED_PLANE_H 1
 
+#include "box.h"
+#include "sphere.h"
+
 struct Plane {
   vec4 data;
 };
@@ -11,6 +14,19 @@ vec3 normal(Plane a) {
 
 // compute a signed distance to the plane
 float distance(Plane p, vec3 q) { return dot(p.data, vec4(q,1.0)); }
+
+// returns the range of signed distances for an axis aligned bounding box to the plane
+vec2 distance(Plane a, Box b) {
+  bvec3 m = lessThan(normal(a),vec3(0.));
+  return vec2(distance(a,mix(b.hi,b.lo,m)), distance(a,mix(b.lo,b.hi,m)));
+}
+
+// returns the range of signed distances for a bounding sphere to the plane
+vec2 distance(Plane a, Sphere b) {
+  float d = distance(a,position(b));
+  float r = radius(b);
+  return vec2(d-r,d+r);
+}
 
 // project a point onto a plane
 vec3 projectToPlane(Plane p, vec3 q) {
