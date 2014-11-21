21e921cdeea5ecfd471a3932bc270cfbefe583f5
Edward Kmett <ekmett@gmail.com>
2014-11-21 05:37:00 -0500

    bitmasked frustum testing, also added sphere test

--- src/Quine/Frustum.hs
+++ src/Quine/Frustum.hs
@@ -7,16 +7,20 @@ module Quine.Frustum
   ) where
 
 import Control.Lens
+import Data.Bits
+import Data.Bits.Lens
 import Data.Data
 import Data.Vector -- .Storable
+import Data.Word
 import GHC.Generics
 import Linear
 import Prelude hiding (any, all)
 import Quine.Bounding.Box
+import Quine.Bounding.Sphere
 import Quine.GL.Types
 import Quine.Plane
 
-data Frustum = Frustum { planes :: Vector Plane, points :: Vector Vec3 }
+data Frustum = Frustum { frustumPlanes :: Vector Plane, frustumPoints :: Vector Vec3 }
   deriving (Show,Eq,Ord,Generic,Typeable,Data)
 
 {-
@@ -34,17 +38,20 @@ buildFrustum origin dir up near far fovy aspect = undefined -- TODO
 -}
 
 instance OverlapsBox Frustum where
-  overlapsBox (Frustum ps qs) b@(Box l h)
-    = all (\p -> signedDistance p (pVertex p b) >= 0) ps
-   && any (\q -> q^._x <= h^._x) qs
-   && any (\q -> q^._y <= h^._y) qs
-   && any (\q -> q^._z <= h^._z) qs
-   && any (\q -> q^._x >= l^._x) qs
-   && any (\q -> q^._y >= l^._y) qs
-   && any (\q -> q^._z >= l^._z) qs
+  overlapsBox (Frustum ps qs) b@(Box (V3 lx ly lz) (V3 hx hy hz))
+    = all (\p -> signedDistance p (pVertex p b) >= 0) ps && foldl' (\r q -> r .|. mask q) (0::Word8) qs == 0x3f
+    where mask (V3 x y z) = 0 & partsOf bits .~ [ x >= lx, y >= ly, z >= lz, x <= hx, y <= hy, z <= hz]
 
 class OverlapsFrustum a where
   overlapsFrustum :: a -> Frustum -> Bool
 
 instance OverlapsFrustum Box where
   overlapsFrustum = flip overlapsBox
+
+-- | In theory a sphere _could_ be large enough to pull the same stunt as a box 
+instance OverlapsSphere Frustum where
+  overlapsSphere (Frustum ps _) (Sphere c r)
+    = all (\p -> signedDistance p c + r >= 0) ps
+
+instance OverlapsFrustum Sphere where
+  overlapsFrustum = flip overlapsSphere
