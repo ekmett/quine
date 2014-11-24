5f270774e72d755ffb4e64fa994868bf160c0d8f
Edward Kmett <ekmett@gmail.com>
2014-11-24 05:42:03 -0500

    switched frustum to 'V' so we can build generic blocks
    * There appears to be an issue with a missing instance?

--- src/Quine/Geometry/Frustum.hs
+++ src/Quine/Geometry/Frustum.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
 module Quine.Geometry.Frustum
   ( Frustum(..)
   , HasFrustum(..)
@@ -11,21 +12,26 @@ module Quine.Geometry.Frustum
 import Control.Lens
 import Data.Bits
 import Data.Bits.Lens
-import Data.Data
-import Data.Vector -- .Storable
+import Data.Vector
 import Data.Word
 import GHC.Generics
 import Linear hiding (frustum)
+import Linear.V
 import Prelude hiding (any, all)
 import Quine.Geometry.Box
 import Quine.Geometry.Plane
 import Quine.Geometry.Sphere
+--import Quine.GL.Block
 import Quine.GL.Types
 
-data Frustum = Frustum { _frustumPlanes :: Vector Plane, _frustumPoints :: Vector Vec3 }
-  deriving (Show,Eq,Ord,Generic,Typeable,Data)
+data Frustum = Frustum { _frustumPlanes :: V 6 Plane, _frustumPoints :: V 8 Vec3 }
+  deriving (Show,Eq,Ord,Generic)
 
 makeClassy ''Frustum
+
+-- | encoded to match shaders/geometry/frustum.h
+-- instance Block Frustum
+  
 
 {-
 -- | @buildFrustum origin direction nearZ farZ fovy aspectRatio@
@@ -42,7 +48,7 @@ buildFrustum origin dir up near far fovy aspect = undefined -- TODO
 -}
 
 instance OverlapsBox Frustum where
-  overlapsBox (Frustum ps qs) b@(Box (V3 lx ly lz) (V3 hx hy hz))
+  overlapsBox (Frustum (V ps) (V qs)) b@(Box (V3 lx ly lz) (V3 hx hy hz))
     = all (\p -> signedDistance p (pVertex p b) >= 0) ps && foldl' (\r q -> r .|. mask q) (0::Word8) qs == 0x3f
     where mask (V3 x y z) = 0 & partsOf bits .~ [ x >= lx, y >= ly, z >= lz, x <= hx, y <= hy, z <= hz]
 
@@ -53,7 +59,7 @@ instance OverlapsFrustum Box where
   overlapsFrustum = flip overlapsBox
 
 instance OverlapsSphere Frustum where
-  overlapsSphere (Frustum ps _) (Sphere c r)
+  overlapsSphere (Frustum (V ps) _) (Sphere c r)
     = all (\p -> signedDistance p c + r >= 0) ps
 
 instance OverlapsFrustum Sphere where
