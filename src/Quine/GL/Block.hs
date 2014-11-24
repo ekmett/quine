4497a422b4cd6f840e03ff9a2826f06fa09eaba8
Edward Kmett <ekmett@gmail.com>
2014-11-24 06:43:41 -0500

    block storage of quaternions and complex numbers

--- src/Quine/GL/Block.hs
+++ src/Quine/GL/Block.hs
@@ -35,6 +35,7 @@ import Control.Applicative
 import Control.Category
 import Control.Lens hiding (to, from)
 import Control.Monad.IO.Class
+import Data.Complex
 import Data.Data
 import Data.Foldable
 import Data.Functor.Rep hiding (Rep)
@@ -854,6 +855,58 @@ instance Block DMat4 where
     pokeByteOff p (o + 64) (V4 c g k q)
     pokeByteOff p (o + 96) (V4 d h l r)
   read430 = read140
+  write430 = write140
+
+instance Block (Complex Float) where
+  alignment140 _ = 16
+  sizeOf140    _ = 16
+  alignment430 _ = 8
+  sizeOf430    _ = 8
+  isStruct     _ = True
+  read140 p (Offset o) = liftIO $ do
+    V2 a b <- peekByteOff p o
+    return $ a :+ b
+  write140 p (Offset o) (a :+ b) = liftIO $ pokeByteOff p o (V2 a b)
+  read430  = read140
+  write430 = write140
+
+instance Block (Complex Double) where
+  alignment140 _ = 16
+  sizeOf140    _ = 16
+  alignment430   = alignment140
+  sizeOf430      = sizeOf140
+  isStruct     _ = True
+  read140 p (Offset o) = liftIO $ do
+    V2 a b <- peekByteOff p o
+    return $ a :+ b
+  write140 p (Offset o) (a :+ b) = liftIO $ pokeByteOff p o (V2 a b)
+  read430  = read140
+  write430 = write140
+
+instance Block (Quaternion Float) where
+  alignment140 _ = 16
+  sizeOf140    _ = 16
+  alignment430   = alignment140
+  sizeOf430      = sizeOf140
+  isStruct     _ = True
+  read140 p (Offset o) = liftIO $ do
+    V4 a b c d <- peekByteOff p o
+    return $ Quaternion d (V3 a b c)
+  write140 p (Offset o) (Quaternion d (V3 a b c)) = liftIO $ pokeByteOff p o (V4 a b c d)
+  read430  = read140
+  write430 = write140
+
+instance Block (Quaternion Double) where
+  alignment140 _ = 32
+  sizeOf140    _ = 32
+  alignment430   = alignment140
+  sizeOf430      = sizeOf140
+  isStruct     _ = True
+  read140 p (Offset o) = liftIO $ do
+    V4 a b c d <- peekByteOff p o
+    return $ Quaternion d (V3 a b c)
+  write140 p (Offset o) (Quaternion d (V3 a b c)) = liftIO $ pokeByteOff p o (V4 a b c d)
+  read430  = read140
   write430 = write140
 
 instance (Dim n, Block a) => Block (V n a) where
