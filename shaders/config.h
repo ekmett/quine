46791247f5e61fc0b7903a23a8bdde52c9191c39
Edward Kmett <ekmett@gmail.com>
2014-11-28 19:41:41 -0500

    started moving things out to a config.h that can be imported from Haskell as well

---
+++ shaders/config.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDED_CONFIG_H
+#define INCLUDED_CONFIG_H
+
+// the maximum number of viewports we'll use
+//
+// used in layout() context, so we can't use a const int.
+#define MAX_VIEWPORTS 16
+
+
+#endif
