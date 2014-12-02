7dccf6e4c949004bcc5e7e1130177497dc5b3abb
Jan-Philip Loos <maxdaten@gmail.com>
2014-12-02 20:26:43 +0100

    init Quine.GL.ProgramPipeline module

---
+++ src/Quine/GL/ProgramPipeline.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+--------------------------------------------------------------------
+-- |
+-- Copyright :  (c) 2014 Edward Kmett and Jan-Philip Loos
+-- License   :  BSD2
+-- Maintainer:  Edward Kmett <ekmett@gmail.com>
+-- Stability :  experimental
+-- Portability: non-portable
+--
+-- Separable 'Programs's can be combined to a 'ProgramPipeline'
+-- Requires: 4.1+ or GL_ARB_separate_shader_objects
+--------------------------------------------------------------------
+module Quine.GL.ProgramPipeline
+  ( ProgramPipeline
+  , PipelineStage
+  , boundProgramPipeline
+  ) where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Coerce
+import Data.Data
+import Data.Default
+import Foreign.Marshal.Alloc
+import Foreign.Marshal.Array
+import Foreign.Storable
+import GHC.Generics
+import Graphics.GL.Core45
+import Graphics.GL.Types
+import Quine.StateVar
+import Quine.GL.Object
+
+-- | A Pipeline object captures shader stages
+newtype ProgramPipeline = ProgramPipeline GLuint deriving (Eq,Ord,Show,Read,Typeable,Data,Generic)
+
+type PipelineStage = GLbitfield
+
+instance Object ProgramPipeline where
+  object = coerce
+  isa i = (GL_FALSE /=) `liftM` glIsProgramPipeline (coerce i)
+  deletes xs = liftIO $ allocaArray n $ \p -> do
+    pokeArray p (coerce xs)
+    glDeleteProgramPipelines (fromIntegral n) p
+    where n = length xs
+
+instance Gen ProgramPipeline where
+  gens n = liftIO $ allocaArray n $ \p -> do
+    glGenProgramPipelines (fromIntegral n) p
+    map ProgramPipeline <$> peekArray n p
+
+instance Default ProgramPipeline where
+  def = ProgramPipeline 0
+
+boundProgramPipeline :: StateVar ProgramPipeline
+boundProgramPipeline = StateVar g s where
+  g = fmap (ProgramPipeline . fromIntegral) $ alloca $ liftM2 (>>) (glGetIntegerv GL_PROGRAM_PIPELINE_BINDING) peek
+  s = glBindProgramPipeline . coerce
