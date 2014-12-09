a5094e925a4601fffe4227bc945d0cdfbe2153a3
Oliver Charles <ollie@ocharles.org.uk>
2014-12-09 23:43:05 +0000

    Image: Swap GL_UNPACK with GL_PACK

--- src/Quine/Image.hs
+++ src/Quine/Image.hs
@@ -117,14 +117,14 @@ download :: forall m a. (MonadIO m, ImageFormat a) => Int -> Int -> Int -> Int -
 download x y w h
   | w >= 0, h >= 0, n <- w * h = liftIO $ do
     fp <- mallocForeignPtrArray (compSize2D 1 fmt typ w h)
-    glPixelStorei GL_UNPACK_LSB_FIRST    0
-    glPixelStorei GL_UNPACK_SWAP_BYTES   0
-    glPixelStorei GL_UNPACK_ROW_LENGTH   0
-    glPixelStorei GL_UNPACK_IMAGE_HEIGHT 0
-    glPixelStorei GL_UNPACK_SKIP_ROWS    0
-    glPixelStorei GL_UNPACK_SKIP_PIXELS  0
-    glPixelStorei GL_UNPACK_SKIP_IMAGES  0
-    glPixelStorei GL_UNPACK_ALIGNMENT    1 -- normally 4!
+    glPixelStorei GL_PACK_LSB_FIRST    0
+    glPixelStorei GL_PACK_SWAP_BYTES   0
+    glPixelStorei GL_PACK_ROW_LENGTH   0
+    glPixelStorei GL_PACK_IMAGE_HEIGHT 0
+    glPixelStorei GL_PACK_SKIP_ROWS    0
+    glPixelStorei GL_PACK_SKIP_PIXELS  0
+    glPixelStorei GL_PACK_SKIP_IMAGES  0
+    glPixelStorei GL_PACK_ALIGNMENT    1 -- normally 4!
     withForeignPtr fp $ glReadPixels (fromIntegral x) (fromIntegral y) (fromIntegral w) (fromIntegral h) fmt typ . castPtr
     return $ Image w h $ V.unsafeFromForeignPtr fp 0 n
   | otherwise = error "download: bad size"
@@ -134,14 +134,14 @@ download x y w h
 -- | @'downloadM' x0 y0@ copies the screen starting at position (x,y) into an existing mutable image
 downloadM :: forall m a. (MonadIO m, ImageFormat a) => Int -> Int -> MutableImage RealWorld a -> m ()
 downloadM x y (MutableImage w h mv) = liftIO $ do
-    glPixelStorei GL_UNPACK_LSB_FIRST    0
-    glPixelStorei GL_UNPACK_SWAP_BYTES   0
-    glPixelStorei GL_UNPACK_ROW_LENGTH   0
-    glPixelStorei GL_UNPACK_IMAGE_HEIGHT 0
-    glPixelStorei GL_UNPACK_SKIP_ROWS    0
-    glPixelStorei GL_UNPACK_SKIP_PIXELS  0
-    glPixelStorei GL_UNPACK_SKIP_IMAGES  0
-    glPixelStorei GL_UNPACK_ALIGNMENT    1 -- normally 4!
+    glPixelStorei GL_PACK_LSB_FIRST    0
+    glPixelStorei GL_PACK_SWAP_BYTES   0
+    glPixelStorei GL_PACK_ROW_LENGTH   0
+    glPixelStorei GL_PACK_IMAGE_HEIGHT 0
+    glPixelStorei GL_PACK_SKIP_ROWS    0
+    glPixelStorei GL_PACK_SKIP_PIXELS  0
+    glPixelStorei GL_PACK_SKIP_IMAGES  0
+    glPixelStorei GL_PACK_ALIGNMENT    1 -- normally 4!
     MV.unsafeWith mv $ glReadPixels
       (fromIntegral x) (fromIntegral y)
       (fromIntegral w) (fromIntegral h)
@@ -155,27 +155,27 @@ class Image2D i where
 
 instance ImageFormat a => Image2D (Image a) where
   upload i@(Image w h v) t l = liftIO $ do
-    glPixelStorei GL_PACK_LSB_FIRST    0
-    glPixelStorei GL_PACK_SWAP_BYTES   0
-    glPixelStorei GL_PACK_ROW_LENGTH   0
-    glPixelStorei GL_PACK_IMAGE_HEIGHT 0
-    glPixelStorei GL_PACK_SKIP_ROWS    0
-    glPixelStorei GL_PACK_SKIP_PIXELS  0
-    glPixelStorei GL_PACK_SKIP_IMAGES  0
-    glPixelStorei GL_PACK_ALIGNMENT    1 -- normally 4!
+    glPixelStorei GL_UNPACK_LSB_FIRST    0
+    glPixelStorei GL_UNPACK_SWAP_BYTES   0
+    glPixelStorei GL_UNPACK_ROW_LENGTH   0
+    glPixelStorei GL_UNPACK_IMAGE_HEIGHT 0
+    glPixelStorei GL_UNPACK_SKIP_ROWS    0
+    glPixelStorei GL_UNPACK_SKIP_PIXELS  0
+    glPixelStorei GL_UNPACK_SKIP_IMAGES  0
+    glPixelStorei GL_UNPACK_ALIGNMENT    1 -- normally 4!
     V.unsafeWith v $ glTexImage2D t l (internalFormat i) (fromIntegral w) (fromIntegral h) 0 (pixelFormat i) (pixelType i) . castPtr
     swizzle i t
 
 instance (ImageFormat a, s ~ RealWorld) => Image2D (MutableImage s a) where
   upload i@(MutableImage w h v) t l = liftIO $ do
-    glPixelStorei GL_PACK_LSB_FIRST    0
-    glPixelStorei GL_PACK_SWAP_BYTES   0
-    glPixelStorei GL_PACK_ROW_LENGTH   0
-    glPixelStorei GL_PACK_IMAGE_HEIGHT 0
-    glPixelStorei GL_PACK_SKIP_ROWS    0
-    glPixelStorei GL_PACK_SKIP_PIXELS  0
-    glPixelStorei GL_PACK_SKIP_IMAGES  0
-    glPixelStorei GL_PACK_ALIGNMENT    1 -- normally 4!
+    glPixelStorei GL_UNPACK_LSB_FIRST    0
+    glPixelStorei GL_UNPACK_SWAP_BYTES   0
+    glPixelStorei GL_UNPACK_ROW_LENGTH   0
+    glPixelStorei GL_UNPACK_IMAGE_HEIGHT 0
+    glPixelStorei GL_UNPACK_SKIP_ROWS    0
+    glPixelStorei GL_UNPACK_SKIP_PIXELS  0
+    glPixelStorei GL_UNPACK_SKIP_IMAGES  0
+    glPixelStorei GL_UNPACK_ALIGNMENT    1 -- normally 4!
     MV.unsafeWith v $ glTexImage2D t l (internalFormat i) (fromIntegral w) (fromIntegral h) 0 (pixelFormat i) (pixelType i) . castPtr
     swizzle i t
 
